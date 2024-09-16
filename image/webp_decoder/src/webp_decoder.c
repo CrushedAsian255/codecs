8c4612c512a4bbe462a738d98a0c7b8151a096e8
CrushedAsian255 <155333241+CrushedAsian255@users.noreply.github.com>
2024-09-16 12:26:04 +1000

    [webp] Refactor reading the image data, fix off-by-1 error.

--- image/webp_decoder/src/webp_decoder.c
+++ image/webp_decoder/src/webp_decoder.c
@@ -3,8 +3,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define err(x) {puts(x);exit(1);}
-#define assert(x,y) {if(!(x))err(y)}
+#define err(x) {printf("Error at line %d: %s\n",__LINE__,x);exit(1);}
+#define assert(x,y) {if(!(x)){printf("Assertion failure at line %d: %s\n",__LINE__,y);exit(1);}}
+#define todo(x) {printf("Not implemented at line %d: %s\n",__LINE__,x);exit(1);}
+
+#define ceil_div(n,d) (((n)+(d)-1)/(d));
+
+typedef uint32_t pixel_t;
 
 typedef struct {
     uint8_t* data;
@@ -24,7 +29,7 @@ uint64_t read_bits(BitstreamState* state, uint8_t bit_count) {
 }
 
 typedef struct {
-    uint32_t* data;
+    pixel_t* data;
     uint16_t width;
     uint16_t height;
 } ImageData;
@@ -45,6 +50,7 @@ void write_image(const ImageData* image_data, const char* output_file_name) {
             break;
         }
     }
+    printf("Image %s: %d x %d%s\n",output_file_name,image_data->width,image_data->height,has_alpha?" with alpha":"");
     char* output_name_buffer = malloc(strlen(output_file_name) + 20);
     
     sprintf(output_name_buffer,"%s.ppm",output_file_name);
@@ -70,11 +76,6 @@ typedef enum {
     COLOR_INDEXING_TRANSFORM
 } WebPTransformType;
 
-typedef struct {
-    WebPTransformType transform_type;
-    void* transform_data;
-} WebPTransform;
-
 const char* transform_names[4] = {
     "Predictor",
     "Colour",
@@ -82,39 +83,20 @@ const char* transform_names[4] = {
     "Colour Index"
 };
 
-void decode_prefix_codes(BitstreamState* bitstream, uint32_t prefix_code_groups, uint32_t color_cache_size) {
-    for(int i = 0; i < prefix_code_groups*5; i++) {
-        uint8_t prefix_code_type = read_bit(bitstream);
-        if(prefix_code_type) {
-            uint8_t num_code_lengths = 4 + read_bits(bitstream,4);
-            printf("%d\n",num_code_lengths);
-            int kCodeLengthCodeOrder[19] = {
-                17, 18, 0, 1, 2, 3, 4, 5, 16, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
-            };
-            int code_length_code_lengths[19] = { 0 };
-            for (i = 0; i < num_code_lengths; ++i) {
-                code_length_code_lengths[kCodeLengthCodeOrder[i]] = read_bits(bitstream,3);
-                printf("%d: %d\n",i,code_length_code_lengths[i]);
-            }
-            uint32_t max_alphabet_size = (i%5==0)?(256+24+color_cache_size):(i%5==4?40:256);
-            uint32_t max_symbol = max_alphabet_size;
-            if(read_bit(bitstream)) {
-                int length_nbits = 2 + 2 * read_bits(bitstream,3);
-                max_symbol = 2 + read_bits(bitstream,length_nbits);
-            }
-            printf("%d %d\n",max_alphabet_size,max_symbol);
-        }
-        break;
-    }
-}
-
-void decode_entropy_coded_image(BitstreamState* bitstream, ImageData* image) {
+void decode_image(BitstreamState* bitstream, ImageData* image, bool is_main_image) {
     uint32_t colour_cache_size = 0;
     if(read_bit(bitstream)) {
         colour_cache_size = 1<<read_bits(bitstream,4);
     }
-    printf("Colour cache: %d\n",colour_cache_size);
-    decode_prefix_codes(bitstream,1,colour_cache_size);
+    pixel_t* colour_cache = malloc(4*colour_cache_size);
+    printf("Colour cache size: %d\n",colour_cache_size);
+
+    uint32_t prefix_group_count = 1;
+
+    if(is_main_image && read_bit(bitstream)) {
+        todo("Read full bitstream");
+    }
+    free(colour_cache);
 }
 
 int main(int argc, char* argv[]) {
@@ -139,42 +121,44 @@ int main(int argc, char* argv[]) {
     assert(read_bits(&file,32)==(*(uint32_t*)"VP8L"),"Error: not a lossless WebP"); 
     assert(read_bits(&file,32)==file_length-20,"Error: invalid WebP header");    
     assert(read_bits(&file,8)==0x2f,"Error: invalid WebP header");  
-    uint16_t image_width = read_bits(&file,14);
-    uint16_t image_height = read_bits(&file,14);
+    uint16_t image_width = read_bits(&file,14) + 1;
+    uint16_t image_height = read_bits(&file,14) + 1;
     uint8_t use_alpha = read_bits(&file,1);
     printf("Image dimensions: %d x %d %s\n",image_width,image_height,use_alpha?"with alpha":"");   
     assert(read_bits(&file,3)==0,"Error: invalid WebP version");
-    WebPTransform transforms[4];
+    WebPTransformType transforms[4];
     uint8_t transform_count = 0;
+
+    uint32_t transform_predictor_block_size = 0;
+    ImageData transform_predictor_subimage;
+
     while(read_bit(&file)) {
         assert(transform_count < 4,"Error: too many image transforms");
         WebPTransformType transform_type = read_bits(&file,2);
         printf("Transform %s\n",transform_names[transform_type]);
-        void* transform_data;
         switch(transform_type) {
             case SUBTRACT_GREEN_TRANSFORM:
                 break;
             case PREDICTOR_TRANSFORM: {
-                uint32_t block_size = (1<<(read_bits(&file,3)+2));
-                uint32_t subimage_width = (image_width + block_size - 1) / block_size;
-                uint32_t subimage_height = (image_height + block_size - 1) / block_size;
-                ImageData subimage = malloc_new_image(subimage_width,subimage_height);
-                decode_entropy_coded_image(&file,&subimage);
-                write_image(&subimage,"predictors");
+                transform_predictor_block_size = (1<<(read_bits(&file,3)+2));
+                uint32_t subimage_width = ceil_div(image_width,transform_predictor_block_size);
+                uint32_t subimage_height = ceil_div(image_height,transform_predictor_block_size);
+                transform_predictor_subimage = malloc_new_image(subimage_width,subimage_height);
+                decode_image(&file,&transform_predictor_subimage,false);
+                write_image(&transform_predictor_subimage,"transform_predictor");
             }
             default:
                 printf("Not implemented transform: %s\n",transform_names[transform_type]);
                 exit(1);
         }
-        transforms[transform_count].transform_type = transform_type;
-        transforms[transform_count].transform_data = transform_data;
+        transforms[transform_count] = transform_type;
         transform_count++;
     }
     ImageData image = malloc_new_image(image_width,image_height);
     for(int i = transform_count-1; i >= 0; i--) {
-        switch(transforms[i].transform_type) {
+        switch(transforms[i]) {
             default:
-                printf("Not implemented transform: %s\n",transform_names[transforms[i].transform_type]);
+                printf("Not implemented transform: %s\n",transform_names[transforms[i]]);
                 exit(1);
         }
     }
